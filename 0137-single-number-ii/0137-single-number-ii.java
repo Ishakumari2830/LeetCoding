e9803e50a5975b0241b7be3169ed1f4d7e913155
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2024-04-06 13:57:29 +0530

    Time: 3 ms (67.61%), Space: 44.6 MB (98.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,21 +1,38 @@
 class Solution {
     public int singleNumber(int[] nums) {
-     HashMap<Integer,Integer> mpp = new HashMap<>();
-     for(int i =0;i<nums.length;i++){
-        if(mpp.containsKey(nums[i])){
-            mpp.put(nums[i],mpp.get(nums[i])+1);
-        }
-        else
-        mpp.put(nums[i],1);
-     }
-    //  for (Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
-    //         if (entry.getValue() == 1) {
-    //             return entry.getKey();
-    //         }
+    //  HashMap<Integer,Integer> mpp = new HashMap<>();
+    //  for(int i =0;i<nums.length;i++){
+    //     if(mpp.containsKey(nums[i])){
+    //         mpp.put(nums[i],mpp.get(nums[i])+1);
+    //     }
+    //     else
+    //     mpp.put(nums[i],1);
+    //  }
+    // //  for (Map.Entry<Integer, Integer> entry : mpp.entrySet()) {
+    // //         if (entry.getValue() == 1) {
+    // //             return entry.getKey();
+    // //         }
+    // // }
+    // for(int key: mpp.keySet()){
+    //     if(mpp.get(key)==1) return key;
     // }
-    for(int key: mpp.keySet()){
-        if(mpp.get(key)==1) return key;
+    // return -1;
+
+    //using bit manipulation 
+
+    int ans =0;
+    int n = nums.length;
+    for(int biti =0;biti<32;biti++){//traversing the 32 bit 
+       int cnt =0;
+        for(int i =0;i<n;i++){//traversing the array
+           if((nums[i] & (1<<biti)) !=0) cnt++; // check if the ith bit is set or not  
+        }
+        if(cnt%3==1){
+            ans= ans | (1<< biti); //set the ith bit
+        }
     }
-    return -1;
+    return ans;
+
+    
 }
 }
