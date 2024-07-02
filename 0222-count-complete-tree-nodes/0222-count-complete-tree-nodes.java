e642e10dd68119a760fd31ba88ed5518ab5901a6
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2024-07-02 21:59:38 +0530

    Time: 0 ms (100%), Space: 47.3 MB (66.82%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,19 +14,45 @@
  * }
  */
 class Solution {
-     private static class Counter {//use of wrapper class since in java passes arg by value
-        int count = 0;
-    }
+    //  private static class Counter {//use of wrapper class since in java passes arg by value
+    //     int count = 0;
+    // }
 
-    private void inorder(TreeNode root, Counter cnt){
-        if(root == null)return;
-        cnt.count++;
-        inorder(root.left,cnt);
-        inorder(root.right,cnt);
+    //private void inorder(TreeNode root, Counter cnt){
+    //     if(root == null)return;
+    //     cnt.count++;
+    //     inorder(root.left,cnt);
+    //     inorder(root.right,cnt);
+    // }
+     public int countNodes(TreeNode root) {
+    //     Counter cnt = new Counter();
+    //     inorder(root,cnt);
+    //     return cnt.count;
+
+    //more optimised one , we know total node = 2^h - 1
+    if(root==null)return 0;
+    int left = getHeightOfLeft(root);
+    int right = getHeightOfRight(root);
+    if(left==right )return ((2<<(left))-1);
+    else return countNodes(root.left)+countNodes(root.right) + 1;
+
+
+
     }
-    public int countNodes(TreeNode root) {
-        Counter cnt = new Counter();
-        inorder(root,cnt);
-        return cnt.count;
+    public int getHeightOfLeft(TreeNode root){
+        int count = 0;
+        while(root.left != null){
+            count ++;
+            root = root.left;
+        }
+        return count;
+    }
+    public int getHeightOfRight(TreeNode root){
+        int cnt =0;
+        while(root.right!=null){
+            cnt++;
+            root = root.right;
+        }
+        return cnt;
     }
 }
