050af48bbeea6d4cbbe99100a4e7980235e0d2d7
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2024-07-22 22:07:32 +0530

    Time: 6 ms (94.61%), Space: 45.2 MB (67.94%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -1,36 +1,15 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
-        int n = names.length;
-        Person[] people = new Person[n];
-
-        // Create an array of Person objects
-        for (int i = 0; i < n; i++) {
-            people[i] = new Person(names[i], heights[i]);
+      Map<Integer,String> map = new TreeMap<>();
+        String res[] = new String[names.length];
+        for(int i=0; i<names.length; i++)
+        {
+            map.put(heights[i],names[i]);
         }
-
-        // Sort the array by height in descending order
-        Arrays.sort(people, new Comparator<Person>() {
-            @Override
-            public int compare(Person p1, Person p2) {
-                return Integer.compare(p2.height, p1.height);
-            }
-        });
-
-        // Extract the sorted names
-        String[] sortedNames = new String[n];
-        for (int i = 0; i < n; i++) {
-            sortedNames[i] = people[i].name;
+        int j = res.length-1;
+        for(Map.Entry<Integer,String> m : map.entrySet()){
+            res[j--] = m.getValue();
         }
-
-        return sortedNames;
+        return res;
     }
-     static class Person {
-        String name;
-        int height;
-
-        Person(String name, int height) {
-            this.name = name;
-            this.height = height;
-        }
     }
-}
