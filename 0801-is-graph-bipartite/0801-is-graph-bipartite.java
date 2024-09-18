b1acd11a73dc638cdd7c6f731cda8a6991828935
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2024-09-18 15:30:39 +0530

    Time: 7 ms (13.02%), Space: 45.3 MB (13.25%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.java
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.java
@@ -35,7 +35,7 @@ class Solution {
             }
         }
         
-        int color[]=new int[n+1];
+        int color[]=new int[n];
         Arrays.fill(color,-1);
 
         for(int i=0;i<n;i++){
