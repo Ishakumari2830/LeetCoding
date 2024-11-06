433596f8500f97e670daa35acf8791f064f0396a
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2024-11-07 01:22:32 +0530

    Time: 6 ms (22.22%), Space: 44.7 MB (21.25%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.java
@@ -2,10 +2,12 @@ class Solution {
     public int setbit(int num) {
         int count = 0;
         while (num != 0) {
-            count += num & 1;
-            num >>= 1;
+            count++;
+           num = num & num -1;
+           
         }
         return count;
+       
     }
     public boolean canSortArray(int[] nums) {
     
