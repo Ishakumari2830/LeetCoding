a6a301f5808e566e3e11a0145657f3260e4b72dc
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-01-07 01:41:15 +0530

    Time: 3 ms (94.95%), Space: 44.5 MB (86.93%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,13 +1,38 @@
 class Solution {
     public int[] minOperations(String boxes) {
+        //brute force : manually
+        // int[] ans = new int[boxes.length()];
+        // for(int i = 0;i<boxes.length();i++){
+        //     if(boxes.charAt(i)=='1'){
+        //         for(int j = 0;j<boxes.length();j++){
+        //             ans[j] += Math.abs(j-i);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        //optimized : prefix sum
+        int n = boxes.length();
         int[] ans = new int[boxes.length()];
-        for(int i = 0;i<boxes.length();i++){
-            if(boxes.charAt(i)=='1'){
-                for(int j = 0;j<boxes.length();j++){
-                    ans[j] += Math.abs(j-i);
-                }
-            }
-        }
-        return ans;
+        int cumValue = 0;
+        int cumSum = 0;
+
+        for(int i = 0;i<n;i++){
+            ans[i]=cumSum;
+            cumValue += boxes.charAt(i)=='0'?0:1;
+            cumSum += cumValue;
+
+      }
+      cumValue = 0;
+     cumSum = 0;
+
+       for(int i = n-1;i>=0;i--){
+            ans[i] +=cumSum;
+            cumValue += boxes.charAt(i)=='0'?0:1;
+            cumSum += cumValue;
+
+      }
+      return ans;
     }
+
 }
