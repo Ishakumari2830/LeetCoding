d7b3d6c994976840c294fcb05e74687274810aae
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-03-26 17:13:50 +0530

    Time: 0 ms (100%), Space: 42.9 MB (46.46%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,39 +1,19 @@
 class Solution {
-   // static void find(int ind,int[] nums,List<Integer> ds,List<List<Integer>> ans){
-    //     if(ind == nums.length){
-    //         ans.add(new ArrayList(ds));
-    //         return;
-    //     }
-    //     ds.add(nums[ind]);
-    //     find(ind+1,nums,ds,ans);
-    //     ds.remove(ds.size()-1);
-    //     find(ind+1,nums,ds,ans);
-        
-
-    // }
-
-     public List<List<Integer>> subsets(int[] nums) {
-    //     List<List<Integer>> ans = new ArrayList<>();
-    //     find(0,nums,new ArrayList<>(),ans);
-    //     return ans;
-        
-
-
-        //using bit manipulation
-         List<Integer> lst;
-List<List<Integer>> ans = new ArrayList<>();
-int n = nums.length;
-int subsets = 1 << n;
-for (int num = 0; num < subsets; num++) {
-    lst = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-        if ((num & (1 << i)) != 0) {
-            lst.add(nums[i]);
+    private void solve(int ind ,List<Integer> al, int[] nums, List<List<Integer>> ans ){
+        if(ind == nums.length){
+            ans.add(new ArrayList<>(al));
+            return;
         }
-    }
-    ans.add(lst);
-}
-return ans;
+        al.add(nums[ind]);
+        solve(ind+1,al,nums,ans);
+        al.remove(al.size()-1);
+        solve(ind+1,al,nums,ans);
 
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> al = new ArrayList<>();
+        solve( 0 , al , nums, ans);
+        return ans;
     }
 }
