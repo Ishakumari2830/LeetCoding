c8b73f2359e28a87d36ee6c3b3d87c657db38ac7
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-06-30 21:08:36 +0530

    Time: 1 ms (99.9%), Space: 44.8 MB (46.29%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,28 +1,26 @@
 class Solution {
-    private void solve(int ind, int[] arr,int target,List<Integer> al, List<List<Integer>> ans){
-        if(ind == arr.length){
-            if(target == 0){
-                ans.add(new ArrayList<>(al));
-
-            }
-            return;
-        }
-        if(arr[ind] <= target){
-            al.add(arr[ind]);
-            solve(ind,arr,target - arr[ind],al,ans);
-            al.remove(al.size()-1);
-            
-        }
-        solve(ind+1,arr,target,al,ans);
-
-
-    }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        int n = candidates.length;
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> al = new ArrayList<>();
-        solve(0,candidates,target,al,ans);
+        List<Integer> ds = new ArrayList<>();
+        solve(0, candidates, target, ans,ds);
         return ans;
+    }
+    public void solve(int ind ,int[] candidates, int target,List<List<Integer>> ans ,  List<Integer> ds ){
+        int n = candidates.length;
+        if(ind == n){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+             return ;
 
+
+        }
+        if(candidates[ind] <= target){
+            ds.add(candidates[ind]);
+            solve(ind ,candidates, target-candidates[ind], ans,ds);
+            ds.remove(ds.size()-1);
+        }
+       // ds.remove(candidates[ind]);
+        solve(ind +1 , candidates, target, ans,ds);
     }
 }
