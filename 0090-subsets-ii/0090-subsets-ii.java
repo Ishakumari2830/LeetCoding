f0ee15f32523eb3ea137b1ba0d4ec71c209fd6b1
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-07-01 23:48:12 +0530

    Time: 1 ms (99.9%), Space: 43.6 MB (61.11%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,18 +1,19 @@
 class Solution {
     private void solve(int ind , List<Integer> al,int[] nums,List<List<Integer>> ans){
    
+       
             ans.add(new ArrayList<>(al));
-            
-        for(int i = ind ;i<nums.length ;i++){
+        
+        //whenever it is asked to not to have duplicate use loop in sorted arr and if there is same adj element continue just like combination sum 2
+        for (int i = ind ; i < nums.length ;i++){
             if(i != ind && nums[i] == nums[i-1])continue;
             al.add(nums[i]);
             solve(i +1 , al,nums,ans);
             al.remove(al.size()-1);
-            //solve(i+1,al,nums,ans);
-
+        }
 
         }
-    }
+    
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
         List<Integer> al = new ArrayList<>();
