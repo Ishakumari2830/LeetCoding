646cfbffa6ce21fbd0cfdc4ded08b92b2092d6aa
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-07-16 00:08:45 +0530

    Time: 0 ms (100%), Space: 40.5 MB (38.2%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,14 +1,24 @@
 class Solution {
     public int fib(int n) {
-        int[] t = new int[n+1];
-        for(int i = 0;i<n+1;i++){
-            t[i] = -1;
+        //memoization
+        // int[] t = new int[n+1];
+        // for(int i = 0;i<n+1;i++){
+        //     t[i] = -1;
         
+        // }
+        // if(n <=1)return n;
+        // if( t[n] != -1){
+        //     return t[n];
+        // }
+        // return t[n] = fib(n-1) + fib(n-2);
+
+        //tabulation
+        int[] t = new int[n+1];
+         if (n >= 0) t[0] = 0;
+        if (n >= 1) t[1] = 1;
+        for(int i = 2;i<n+1;i++){
+            t[i] = t[i-1] + t[i-2];
         }
-        if(n <=1)return n;
-        if( t[n] != -1){
-            return t[n];
-        }
-        return t[n] = fib(n-1) + fib(n-2);
+        return t[n];
     }
 }
