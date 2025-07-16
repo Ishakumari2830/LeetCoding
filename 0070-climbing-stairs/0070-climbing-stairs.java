92f6000075568517ec7326a733d43134247f55e6
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-07-16 22:43:50 +0530

    Time: 0 ms (100%), Space: 41.2 MB (5.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -23,11 +23,11 @@ class Solution {
     //Tabulation
     int[] t = new int[n+1];
    if(n <= 2)return n;
-   t[1] = 1;
-   t[2]= 2;
+   t[0] = 1;
+   t[1]= 1;
    
 
-    for(int i = 3;i <= n;i++){
+    for(int i = 2;i <= n;i++){
         t[i] = t[i-1] + t[i-2];
     }
     return t[n];
