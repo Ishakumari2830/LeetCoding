a479e12cda0070f52365e951be906701e2cd4008
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-07-16 22:41:51 +0530

    Time: 0 ms (100%), Space: 40.3 MB (81.63%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,37 @@
 class Solution {
     public int climbStairs(int n) {
-     if(n <= 2)return n ;
-     int[] t = new int[n+1];
-     t[1] = 1;
-     t[2]= 2;
-     for(int i =3 ;i<=n;i++){
-        t[i] =t[i-1] +t[i-2];
-     }
-     return t[n];
+    //recursive --> will give TLE
+    // if(n == 0)return 1;
+    // if(n == 1)return 1;
+    // int left = climbStairs(n-1);//1 step
+    // int right = climbStairs(n-2);//2 step
+    // return left + right ;
+    
+
+    //Memoization -> TLE
+    // int[] t = new int[n+1];
+    // for(int i = 0;i<n+1;i++){
+    //     t[i] = -1;
+    // }
+    // if(n <= 1)return 1;
+    // if(t[n] != -1){
+    //     return t[n];
+        
+    // }
+    // return t[n] = climbStairs(n-1) + climbStairs(n-2);
+
+    //Tabulation
+    int[] t = new int[n+1];
+   if(n <= 2)return n;
+   t[1] = 1;
+   t[2]= 2;
+   
+
+    for(int i = 3;i <= n;i++){
+        t[i] = t[i-1] + t[i-2];
+    }
+    return t[n];
+
 
     }
 }
