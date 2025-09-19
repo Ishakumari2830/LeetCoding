3dc0d64461000251ead8870289aaa2f568771e5e
Isha Kumari <112392770+Ishakumari2830@users.noreply.github.com>
2025-09-19 17:30:22 +0530

    Time: 792 ms (6.06%), Space: 46.3 MB (5.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,44 +1,19 @@
 class Solution {
-    private boolean matches(int[] s1Count, int[] s2Count) {
-        for (int i = 0; i < 26; i++) {
-            if (s1Count[i] != s2Count[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
     public boolean checkInclusion(String s1, String s2) {
-        int n1 = s1.length();
-        int n2 = s2.length();
-        
-        // If s2's length is less than s1's length, no permutation can be found
-        if (n2 < n1) {
-            return false;
-        }
+        char[] arr1 = s1.toCharArray();
+        Arrays.sort(arr1);
+        String sortedS1 = new String(arr1);
+        return checkRec(s2,sortedS1, s1.length(),0);
+    }
 
-        // Create frequency arrays for s1 and the sliding window in s2
-        int[] s1Count = new int[26];
-        int[] s2Count = new int[26];
+    private boolean checkRec(String s2, String sortedS1, int n , int ind){
+        if(ind + n > s2.length())return false;
+        String sub = s2.substring(ind, ind + n);
+        char[] subArr = sub.toCharArray();
+        Arrays.sort(subArr);
+        String sortedSub = new String(subArr);
+        if(sortedSub.equals(sortedS1))return true;
 
-        // Fill the frequency arrays for the first window in s2 and for s1
-        for (int i = 0; i < n1; i++) {
-            s1Count[s1.charAt(i) - 'a']++;
-            s2Count[s2.charAt(i) - 'a']++;
-        }
-
-        // Sliding window over s2 starting from index n1 to n2
-        for (int i = n1; i < n2; i++) {
-            // If the frequency arrays match, we found a valid permutation
-            if (matches(s1Count, s2Count)) {
-                return true;
-            }
-            
-            // Update the sliding window
-            s2Count[s2.charAt(i) - 'a']++;              // Add the new character to the window
-            s2Count[s2.charAt(i - n1) - 'a']--;          // Remove the oldest character from the window
-        }
-
-        // Final check for the last window
-        return matches(s1Count, s2Count); 
+        return checkRec(s2, sortedS1, n ,ind +1);
     }
 }
